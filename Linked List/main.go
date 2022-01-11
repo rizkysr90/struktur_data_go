47534a5a465e596c553c086ea89929e0f9fcfcbd
Rizky Ramadhan <rizkysr90@gmail.com>
2022-01-11 22:54:40 +0700

    insert node at the end

--- Linked List/main.go
+++ Linked List/main.go
@@ -37,23 +37,31 @@ func (l *LinkedList) Prepend(n *Node) {
 	l.length++
 }
 
+func (l *LinkedList) Append(n *Node) {
+	a := l.head
+
+	for a.link != nil {
+		a = a.link
+	}
+	a.link = n
+}
+
 func main() {
-	// Inisialisais Linked List
-	//Node ke 0,pada saat ini head dari myList = nil
 	myList := LinkedList{}
-	//Membuat Node
+
 	node1 := &Node{data: 50}
 	node2 := &Node{data: 51}
 	node3 := &Node{data: 52}
-
-	//Node ke 1,pada saat ini head dari myList = alamat dari node1 dan node1 link ke nil
-	myList.Prepend(node1)
-	//Node ke 2,pada saat ini head dari myList = alamat dari node2 dan node2 refrensi ke node1
+	node4 := &Node{data: 100}
 	myList.Prepend(node2)
-	//Node ke 3,pada saat ini head dari myList = alamat dari node3 dan node3 refrensi ke node2
 	myList.Prepend(node3)
+	myList.Append(node4)
+	myList.Prepend(node1)
 
-	//Akan print alamat yang sama
-	// fmt.Println(node3)
-	fmt.Println(myList.head.data)
+	a := myList.head
+	for a.link != nil {
+		a = a.link
+	}
+	fmt.Println(a.data)
+
 }
